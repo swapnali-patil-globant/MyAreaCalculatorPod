1f1d61d193d25728d3ff15d1b5f29c76e17cb709
swapnali.patil <swapnali.patil@globant.com>
2025-03-19 12:16:18 +0530

    Initial commit

---
+++ AreaCalculator.swift
@@ -0,0 +1,18 @@
+//
+//  AreaCalculator.swift
+//  MyAreaCalculatorPod
+//
+//  Created by Swapnali Patil on 19/03/25.
+//
+
+import Foundation
+
+public class AreaCalculator {
+    public func calculatorSquareArea(side: Double) -> Double {
+        return side * side
+    }
+    
+    public func calculateRectabgleArea(length: Double, breadth: Double) -> Double {
+        return length * breadth
+    }
+}
---
+++ MyAreaCalculatorPod/MyAreaCalculatorPod.h
@@ -0,0 +1,18 @@
+//
+//  MyAreaCalculatorPod.h
+//  MyAreaCalculatorPod
+//
+//  Created by Swapnali Patil on 19/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyAreaCalculatorPod.
+FOUNDATION_EXPORT double MyAreaCalculatorPodVersionNumber;
+
+//! Project version string for MyAreaCalculatorPod.
+FOUNDATION_EXPORT const unsigned char MyAreaCalculatorPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAreaCalculatorPod/PublicHeader.h>
+
+
